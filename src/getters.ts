ac6602480766546add5a6a686dd8296c941b0171
Margherita Magatti <margherita.magatti@cofidis.it>
2024-11-06 11:12:37 +0100

    daily total amounts instead of averages

--- src/getters.ts
+++ src/getters.ts
@@ -19,6 +19,31 @@ const dayAverages = (array: any[], field: string): number[] => {
 }
 
 /**
+ * Given an array of energy data ordered by date and a field (es. 'prod'), 
+ * returns an array of numbers where each number is the daily total amount for that field.
+ * For example, returns the daily amounts of production for each day present in the array.
+ */
+const dailyValuesFromField = (array: any[], field: string): number[] => {
+  if (array.length === 0)
+    return []
+
+  let daySum = 0;
+  const sums: number[] = [];
+  array.forEach((obj, i) => {
+    if(i === 0) {
+      daySum = obj[field] ?? 0
+    } else if (array[i-1].ts.slice(0, 10) === obj.ts.slice(0, 10)) {
+        daySum += obj[field]
+    } else {
+      sums.push(daySum);
+      daySum = obj[field];
+    }
+  })
+
+  return sums;
+}
+
+/**
  * Given an array of energy data and a field (es. 'prod'),
  * returns the sum of all the <field> values.
  */
@@ -32,23 +57,23 @@ const getValueFromField = (array: any[], field: string): number => {
 
 /**
 * Given an array of energy data ordered by date and 2 fields (es. 'prod' and 'toGrid'),
-* returns the daily average percentage of the 2nd field compared to the 1st field:
-* for example, the daily average percentage of produced energy that was injected to the grid. 
+* returns the average percentage of the 2nd field compared to the 1st field by day.
+* For example, the average percentage of daily produced energy that was injected to the grid. 
 */
 const getAverageRateFromFields = (array: any[], field1: string, field2: string): number => {
 
     if (array.length === 0)
         return 0
 
-    const averagesField1 = dayAverages(array, field1)
-    const averagesField2 = dayAverages(array, field2)
+    const dailyField1 = dailyValuesFromField(array, field1)
+    const dailyField2 = dailyValuesFromField(array, field2)
 
-    if(averagesField1.length === 0 || averagesField2.length === 0)
+    if(dailyField1.length === 0 || dailyField2.length === 0 || dailyField1.length !== dailyField2.length)
         return 0
 
-    const rates = averagesField1.map((value1, i) => {
+    const rates = dailyField1.map((value1, i) => {
         if (value1 === 0) return 0
-        return averagesField2[i] * 100 / value1
+        return dailyField2[i] * 100 / value1
     })
 
     let sum = rates.reduce((sum, curr) => {
@@ -68,12 +93,12 @@ const getDayAverageFromField = (array: any[], field: string) => {
     if (array.length === 0)
       return 0
     
-    const averages = dayAverages(array, field)
+    const dailyValues = dailyValuesFromField(array, field)
 
-    if (averages.length < 1)
+    if (dailyValues.length < 1)
         return 0
     
-    let sum = averages.reduce((sum, curr) => {
+    let sum = dailyValues.reduce((sum, curr) => {
       if (!sum || !curr) return 0
       return sum + curr
     })
